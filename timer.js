1fb49441474b15cef4cadb78696c48f0552142d8
centurianomy <rawatchirag333@gmail.com>
2026-03-12 19:10:20 +0530

    timer

---
+++ timer.js
@@ -0,0 +1,37 @@
+//define variables
+
+let timerDisplay = document.getElementById("timer");
+let startBtn = document.getElementById("strt");
+let pauseBtn = document.getElementById("paus");
+let restartBtn = document.getElementById("rstrt");
+
+let count = 0;
+let timer;
+
+//start
+
+startBtn.addEventListener("click", function (){
+	timer = setInterval(function () {
+		count++;
+		timerDisplay.innerText = count;
+
+		if(count == 20){
+			clearInterval(timer);
+		}
+	}, 1000);
+});
+
+
+//pause
+
+pauseBtn.addEventListener("click", function(){
+	clearInterval(timer);
+});
+
+//restart
+
+restartBtn.addEventListener("click", function(){
+	clearInterval(timer);
+	count = 0;
+	timerDisplay.innerText = count;
+});
