15c38d42dcdd838c442bcd3a6be863c3f2abaca9
centurianomy <rawatchirag333@gmail.com>
2026-05-07 00:14:15 +0530

    Big integer to integer conversion test

---
+++ Conversions/1.js
@@ -0,0 +1,7 @@
+// adding bigint and other type
+let x=10n;
+let y=5;
+
+console.log(x+y);
+
+//js auto coversion nhi krta bigint and othe rtypes k beech and this will give type error but we can do it by converting bigint to number or number to bigint
